0af04304f74c5f8fc5b553b93120218b689c4911
@philshem <philshem@pm.me>
2020-07-22 11:52:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // https://stackoverflow.com/a/59621673/2327328
 var chance = require('chance').Chance();
 global.window.chance = chance;
+var plotly = require('plotly.js');
+global.window.Plotly = plotly.Plotly;
 // browserify main.js -o bundle.js
